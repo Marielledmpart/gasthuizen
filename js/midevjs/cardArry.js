d43e8f0e3289e5f8a89b7fd6cdc01bf94da4dab6
namighadri <nami_ghadri@hotmail.com>
2023-08-30 19:51:47 +0200

    adding project files to rep

---
+++ js/midevjs/cardArry.js
@@ -0,0 +1,101 @@
+export var textRegister = "De informatie op deze website over het dagelijks leven in het St. Pietersgasthuis is afkomstig uit\n" +
+    "inventarisnummer 83 van het archief van de Amsterdamse gasthuizen. Het is een register van inkomsten en\n" +
+    "uitgaven dat bijgehouden werd door de middeleeuwse ziekenhuisdirecteuren uit de jaren 1550-1559.\n" +
+    "Financiële administratie lijkt vaak erg saai en onbeduidend. Echter, als we tussen de regels van de\n" +
+    "administratie door lezen krijgen we een kijkje van binnenuit op het dagelijks leven in de gasthuizen en\n" +
+    "kloosters. Zo komen we bijvoorbeeld te weten wat de bewoners van het St. Pietersgasthuis aten en welke\n" +
+    "persoonlijke bezittingen de proveniers meenamen als zij hun kost in het gasthuis kochten. We zien welke\n" +
+    "leveranciers producten en diensten aan de gasthuismeesters leverden en hoeveel medewerkers voor het gasthuis\n" +
+    "aan het werk waren en waaruit hun loon bestond."
+export var textVerhaal = "Het St. Pietersgasthuis was een veel omvangrijker organisatie dan alleen een ziekenhuis. De\n" +
+    "gasthuismeesters bestierden een grote onderneming met een flink vermogen in de vorm van huizen en landerijen, renten en pachten dat aangevuld werd met de donaties en legaten van weldoeners. Het gasthuis was naast de medische\n" +
+    "zorg voor de zieken, vreemdelingen en proveniers in de ziekenzaal en hun geestelijke zorg in de kapel\n" +
+    "actief op het gebied van landbouw en veeteelt, het beschikte over een eigen keuken, brouwerij en bakkerij.\n" +
+    "Daarnaast lag het onderhoud van de gebouwen van het gasthuis in handen van een uitgebreid team van\n" +
+    "timmerlieden, metselaars en leidekkers. Een deel van de medewerkers van het gasthuis was zelfs inwonend,\n" +
+    "zoals de priester die op alle heiligendagen missen in de gasthuiskapel las, voor de zieken preekte enhen de sacramenten toe diende."
+export var textCharitas = "De gasthuizen werden gesteund met schenkingen van rijke en minder rijke Amsterdammers aan hun stadsgenoten\n" +
+    "die de verliezers waren geworden in stedelijke samenleving: armen, zieken, zwervers en vreemdelingen. De\n" +
+    "middeleeuwse Amsterdammers namen hiermee verantwoordelijkheid voor hun hulpbehoevende stadsgenoten.\n" +
+    "Helemaal onbaatzuchtig waren de schenkingen aan het gasthuis meestal niet. Regelmatig lieten Amsterdammers\n" +
+    "in hun testament bedragen aan het gasthuis na om jaarlijks op hun sterfdag een maaltijd voor de arme zieken\n" +
+    "te verzorgen. In ruil voor de schenking zouden de zieken op die dag bidden voor de ziel van hun weldoener.\n" +
+    "Daarnaast was er nog een reden om juist aan het gasthuis te schenken. De primaire taak van de gasthuizen\n" +
+    "leunde namelijk dicht aan tegen de zeven werken van barmhartigheid, zoals het spijzen van de hongerigen, het\n" +
+    "herbergen van vreemdelingen en het verzorgen van de zieken. Het begunstigen van het gasthuis was daarmee een\n" +
+    "manier om de eigen devotie in dienst te stellen van het welzijn van de gemeenschap."
+export var textBoodschapen = "Het St. Pietersgasthuis was een volwaardig bedrijf dat bestond bij de gratie van weldoeners, maar ook zelf inkomsten genereerde. Onder het kopje ‘Coeckenmoer, Hier nae volcht tgeen die moer inde coecken dagelycx ontfanckt van melck smeer hoij ende diergelycken’, registreerden de gasthuismeesters een gemiddeld inkomen uit de keuken van 100 gulden per jaar met uitschieters naar onderen van 64 gulden in 1554  en naar boven van 190 gulden in 1558 . Het ging hier om melk, vet en hooi dat blijkbaar niet nodig was voor de gasthuiskeuken en dus aan derden verkocht kon worden. \n" +
+    "De inkomsten van de keuken vormden echter een druppel op de gloeiende plaat. Wanneer we onze aandacht op de uitgaven van de keuken richten, zien we dat in de jaren 1550-1556 gemiddeld 475 gulden aan boter werd uitgegeven  en tussen 1551-1559 gemiddeld 193 gulden per jaar aan kaas, met een uitschieter in 1558 van 302 gulden . Als vaste uitgaven komen we verder nog de inkoop van rogge en tarwe  tegen en vlees en vis  voor zo’n 230 gulden per jaar. Haring, bokking en rozijnen  vormen later in het register een veel kleinere post. . In de post extra ordinary vinden we kosten voor gort, bonen erwten, haver, azijn en zout."
+export var textEerlickmael = "In het jaar 1551 bespreekt Nel, de weduwe Pieter Colijn, in haar testament jaarlijks een maaltijd met begraden spijs voor de zieke en gezonde mensen in St. Pietersgasthuis op zondag na Pinksteren. De zieken krijgen daarbij nog een stukje witbrood en een pint Rijnse wijn . \n" +
+    "Dit bespreken van maaltijden voor de zieken was een veel voorkomende vorm, waarin welgestelde Amsterdammers een testament nalieten aan het Sint Pieters gasthuis. We zien dit terug in register 83, in de uitgaven onder de noemer ‘maaltyden’  waarin door het jaar heen meerdere van deze ‘besproken’ maaltijden vermeld staan. In veel gevallen bestonden deze maaltijden uit gebraden vlees of spijs. Soms aangevuld met wijn en/of brood. Als we deze omschrijving  vergelijken met het hedendaagse ziekenhuis eten lijken deze 16e eeuwse maaltijden nog zo slecht niet, al moeten we ons goed realiseren dat deze maaltijden geen dagelijkse kost waren.\n"
+export var textDagelijksekost = "De nauwkeurige finaciële administratie van de gasthuismeesters levert ons een inkijkje in het voedselpatroon van het gasthuis op. De zieken kregen bijvoorbeeld op witte donderdag in het jaar 1550 lamsvlees voorgeschoteld en op pasen van 1559 hutspot. Om uit te vinden wat de keukenmoeders de inwonders van het St. Pietersgasthuis dagelijks voorschotelden bladeren we in het register naar de uitgaven van de keukenmoeders. \n" +
+    "Samenvattend lijken vlees en vis, kaas, boter en brood veruit de belangrijkste ingredienten zijn van het voedingspakket dat in het gasthuis aan de zieken werd opgediend. En dat gecombineerd met veel bier. Groenten en fruit hebben een minder prominente plek in het register van inkomsten en uitgaven. Wellicht heeft dit dezelfde reden als het ontbreken van de inkoop van melk, daarin was het gasthuis waarschijnlijk zelfvoorzienend. \n" +
+    "In een ouder register van het St. Pietersgasthuis uit 1474-1498 vermelden de gasthuismeesters wel de aankoop van buiskool  , een bos wortels , appels, mosterdzaad, honing en vijgen  en een moestuin van Sint Pietersgasthuis is terug te vinden in een aantekening  op een kaart die landmeter Pieter Koenenz op verzoek van Sint Pieters gasthuis in 1559 maakte: ‘Item dese coeltuyn off warmoestuijn is gelegen an deser stede cingel achter Boere verdriet’ . Wat men hier, behalve kool, nog meer verbouwde is helaas niet duidelijk.\n"
+export var cardArr = [
+    {
+        page: "pieters",
+        content: [
+            {
+                class: "verhaal",
+                nextClass: "register83",
+                previousClass: "",
+                src: "../../images/midev/verhaal.png",
+                text: textVerhaal,
+                title: "De organisatie St. Pietersgasthuis",
+                alt:"verhaal"
+            },
+            {
+                class: "register83",
+                nextClass: "charitas",
+                previousClass: "verhaal",
+                src: "../../images/midev/register-83.png",
+                text: textRegister,
+                title: "Register 83",
+                alt:"Register 83"
+            },
+            {
+                class: "charitas",
+                src: "../../images/midev/charitas-sm.png",
+                nextClass: "",
+                previousClass: "register83",
+                text: textCharitas,
+                title: "Charitas en zielenheil",
+                alt:"charitas"
+            }
+        ]
+    },
+    {
+        page: "keuken",
+        content: [
+            {
+                class: "boodschapen",
+                nextClass: "eerlickmael",
+                previousClass: "",
+                src: "../../images/midev/keuken/boodschappen.png",
+                text: textBoodschapen,
+                title: "Boodschappen van de keukenmoeders",
+                alt:"Boodschappen"
+            },
+            {
+                class: "eerlickmael",
+                nextClass: "dagelijksekost",
+                previousClass: "boodschapen",
+                src: "../../images/midev/keuken/eerliek-mael.png",
+                text: textEerlickmael,
+                title: "Een eerlick mael.",
+                alt:"Eerlick"
+            },
+            {
+                class: "dagelijksekost",
+                nextClass: "",
+                previousClass: "eerlickmael",
+                src: "../../images/midev/keuken/dagelijkse-kost.png",
+                text: textDagelijksekost,
+                title: "Dagelijkse kost",
+                alt:"Dagelijkse"
+            }
+        ]
+    }
+]
+
+export default
